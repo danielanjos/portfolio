c45a9a1bbd3956e984e7cb356150b2ad048cff19
Daniel Anjos <dmartins.anj@gmail.com>
2020-04-14 07:48:53 -0300

    Deploy Test 06

--- src/pages/Projetos/index.js
+++ src/pages/Projetos/index.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import ImageGallery from 'react-image-gallery';
 import Modal from '@material-ui/core/Modal';
 
@@ -25,89 +25,92 @@ import SafishareMenuImg from '../../assets/safishare/menu.png';
 import SafisharePerguntas1Img from '../../assets/safishare/perguntas1.png';
 import SafisharePerguntas2Img from '../../assets/safishare/perguntas2.png';
 
-const lowcostImages =
-  [
-    {
-      original: LowcostMenu,
-      thumbnail: LowcostMenu,
-    },
-    {
-      original: LowcostCadastraEquipamento,
-      thumbnail: LowcostCadastraEquipamento,
-    },
-    {
-      original: LowcostListaEquipamentos,
-      thumbnail: LowcostListaEquipamentos,
-    },
-    {
-      original: LowcostListaPedidos,
-      thumbnail: LowcostListaPedidos,
-    },
-    {
-      original: LowcostListaNumeradores,
-      thumbnail: LowcostListaNumeradores,
-    },
-    {
-      original: LowcostUsuarios,
-      thumbnail: LowcostUsuarios,
-    },
-    {
-      original: LowcostConferencia,
-      thumbnail: LowcostConferencia,
-    },
-    {
-      original: LowcostConferenciaExcel,
-      thumbnail: LowcostConferenciaExcel,
-    },
-  ];
-
-const safishareImages =
-  [
-    {
-      original: SafishareCadastraPermissaoImg,
-      thumbnail: SafishareCadastraPermissaoImg,
-    },
-    {
-      original: SafishareChecklistImg,
-      thumbnail: SafishareChecklistImg,
-    },
-    {
-      original: SafishareEnvioArquivosImg,
-      thumbnail: SafishareEnvioArquivosImg,
-    },
-    {
-      original: SafishareMenuImg,
-      thumbnail: SafishareMenuImg,
-    },
-    {
-      original: SafisharePerguntas1Img,
-      thumbnail: SafisharePerguntas1Img,
-    },
-    {
-      original: SafisharePerguntas2Img,
-      thumbnail: SafisharePerguntas2Img,
-    },
-  ];
-
 export default class Projetos extends Component {
   constructor(props) {
     super(props);
     this.state = {
       openModal: false,
     }
+
+
+
   }
+
+  lowcostImages =
+    [
+      {
+        original: LowcostMenu,
+        thumbnail: LowcostMenu,
+      },
+      {
+        original: LowcostCadastraEquipamento,
+        thumbnail: LowcostCadastraEquipamento,
+      },
+      {
+        original: LowcostListaEquipamentos,
+        thumbnail: LowcostListaEquipamentos,
+      },
+      {
+        original: LowcostListaPedidos,
+        thumbnail: LowcostListaPedidos,
+      },
+      {
+        original: LowcostListaNumeradores,
+        thumbnail: LowcostListaNumeradores,
+      },
+      {
+        original: LowcostUsuarios,
+        thumbnail: LowcostUsuarios,
+      },
+      {
+        original: LowcostConferencia,
+        thumbnail: LowcostConferencia,
+      },
+      {
+        original: LowcostConferenciaExcel,
+        thumbnail: LowcostConferenciaExcel,
+      },
+    ];
+
+  safishareImages =
+    [
+      {
+        original: SafishareCadastraPermissaoImg,
+        thumbnail: SafishareCadastraPermissaoImg,
+      },
+      {
+        original: SafishareChecklistImg,
+        thumbnail: SafishareChecklistImg,
+      },
+      {
+        original: SafishareEnvioArquivosImg,
+        thumbnail: SafishareEnvioArquivosImg,
+      },
+      {
+        original: SafishareMenuImg,
+        thumbnail: SafishareMenuImg,
+      },
+      {
+        original: SafisharePerguntas1Img,
+        thumbnail: SafisharePerguntas1Img,
+      },
+      {
+        original: SafisharePerguntas2Img,
+        thumbnail: SafisharePerguntas2Img,
+      },
+    ];
 
   modal() {
     return (
       // <div className="fundo-galeria">
-        <div className="conteudo-galeria">
-          <ImageGallery
-            items={this.state.listaImagens}
-            showFullscreenButton={false}
-            showPlayButton={false}
-            showBullets={true}
-          />
-        </div>
+      <div className="conteudo-galeria">
+        <ImageGallery
+          items={this.state.listaImagens}
+          showFullscreenButton={false}
+          showPlayButton={false}
+          showBullets={true}
+        />
+      </div>
       // </div>
     )
   };
@@ -121,26 +124,27 @@ export default class Projetos extends Component {
 
   handleCloseModal = () => {
     this.setState({
-      openModal: false
+      openModal: false,
+      listaImagens: []
     });
   };
 
   render() {
     return (
       <>
-      <Modal
-      open={this.state.openModal}
-      onClose={this.handleCloseModal}
-      >
-        {this.modal()}
-      </Modal>
-      <section className="conteudo dvProjetos">
-        <h2>Projetos</h2>
-        <ul>
-          <li><img src={ProjetoLcCapaImg} onClick={() => this.handleOpenModal(lowcostImages)} alt="Sistema de faturamento da Lowcost" /></li>
-          <li className="logo-safishare"><img src={ProjetoSafishareCapaImg} onClick={() => this.handleOpenModal(safishareImages)} alt="Sistema de faturamento da Lowcost" /></li>
-        </ul>
-      </section>
+        <Modal
+          open={this.state.openModal}
+          onClose={this.handleCloseModal}
+        >
+          {this.modal()}
+        </Modal>
+        <section className="conteudo dvProjetos">
+          <h2>Projetos</h2>
+          <ul>
+            <li><img src={ProjetoLcCapaImg} onClick={() => this.handleOpenModal(this.lowcostImages)} alt="Sistema de faturamento da Lowcost" /></li>
+            <li className="logo-safishare"><img src={ProjetoSafishareCapaImg} onClick={() => this.handleOpenModal(this.safishareImages)} alt="Sistema de faturamento da Lowcost" /></li>
+          </ul>
+        </section>
       </>
     );
   }
