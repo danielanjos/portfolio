ab2e28a404d3534b590b40a479191575d0198238
Daniel Anjos <dmartins.anj@gmail.com>
2020-04-14 07:09:37 -0300

    Updates

---
+++ precache-manifest.73c0a9668d264c32c5e31e4a2440c774.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "808d5788041d48a15ad1224732c9ed20",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3b6e49cac8140c1404a8",
+    "url": "/portfolio/static/css/2.40a775a0.chunk.css"
+  },
+  {
+    "revision": "cfb04f66889701b9580c",
+    "url": "/portfolio/static/css/main.446da1a4.chunk.css"
+  },
+  {
+    "revision": "3b6e49cac8140c1404a8",
+    "url": "/portfolio/static/js/2.ec58f2cc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.ec58f2cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfb04f66889701b9580c",
+    "url": "/portfolio/static/js/main.dce8ee86.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "65d0275d65abde33efe7e20b789012d5",
+    "url": "/portfolio/static/media/cadastraEquipamento.65d0275d.png"
+  },
+  {
+    "revision": "a21037a6d224fa62ca7ec4732d3844ea",
+    "url": "/portfolio/static/media/cadastrapermissao.a21037a6.png"
+  },
+  {
+    "revision": "71337d688f1d10b171f7d0e96e1a94ec",
+    "url": "/portfolio/static/media/checklist.71337d68.png"
+  },
+  {
+    "revision": "6bcab6230ba7d07d7543089af3620ba5",
+    "url": "/portfolio/static/media/conferencia.6bcab623.png"
+  },
+  {
+    "revision": "a3e61bb36f6ddfb302ec3b2956a4eb2f",
+    "url": "/portfolio/static/media/conferenciaExcel.a3e61bb3.png"
+  },
+  {
+    "revision": "c75b1bb46a734909925592a3c39814f7",
+    "url": "/portfolio/static/media/envioarquivos.c75b1bb4.png"
+  },
+  {
+    "revision": "30c0290924aea3039781aae7132fea1f",
+    "url": "/portfolio/static/media/listaEquipamentos.30c02909.png"
+  },
+  {
+    "revision": "bde4411452a6b05fee1d0a9c3e023317",
+    "url": "/portfolio/static/media/listaNumeradores.bde44114.png"
+  },
+  {
+    "revision": "e2458d2957c77f5b9c3207a0b1a7a9a3",
+    "url": "/portfolio/static/media/listaPedidos.e2458d29.png"
+  },
+  {
+    "revision": "dd8c7e2661640c38926b4fee0fa38e6c",
+    "url": "/portfolio/static/media/logo_safishare.dd8c7e26.png"
+  },
+  {
+    "revision": "d42946e2bfcfe0fb3a7f74509e45bc07",
+    "url": "/portfolio/static/media/lowcost-logo.d42946e2.png"
+  },
+  {
+    "revision": "a39067ec11bbfbccb37e1f0d328b8906",
+    "url": "/portfolio/static/media/menu.a39067ec.png"
+  },
+  {
+    "revision": "e45488a18e2d16dbdaad12bc124e7c20",
+    "url": "/portfolio/static/media/menu.e45488a1.png"
+  },
+  {
+    "revision": "b691a1ccfc1928a6c63d90603925bbe0",
+    "url": "/portfolio/static/media/perguntas1.b691a1cc.png"
+  },
+  {
+    "revision": "ada7138f24ac7e98f1535d1f259196ed",
+    "url": "/portfolio/static/media/perguntas2.ada7138f.png"
+  },
+  {
+    "revision": "99c496bb272138f09ff8ac0b6afffbfa",
+    "url": "/portfolio/static/media/usuarios.99c496bb.png"
+  }
+]);
